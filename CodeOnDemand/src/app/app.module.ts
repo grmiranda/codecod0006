9e50f564b3f48317b716137544862ddace7ec072
Gabriel Reis Miranda <gabrielmiranda@codeondemand.com.br>
2017-03-31 17:42:56 -0300

    modularização land page

--- CodeOnDemand/src/app/app.module.ts
+++ CodeOnDemand/src/app/app.module.ts
@@ -8,11 +8,17 @@ import { AppComponent } from './app.component';
 // bootstrap modules
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { LandPageComponent } from './pages/land-page/land-page.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { EquipeComponent } from './components/equipe/equipe.component';
+import { FaleConoscoComponent } from './components/fale-conosco/fale-conosco.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandPageComponent,
+    SobreComponent,
+    EquipeComponent,
+    FaleConoscoComponent,
   ],
   imports: [
     BrowserModule,
--- CodeOnDemand/src/app/pages/land-page/land-page.component.ts
+++ CodeOnDemand/src/app/pages/land-page/land-page.component.ts
@@ -10,6 +10,7 @@ export class LandPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
